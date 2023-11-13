eec16fb17d6f8b34ad06b5284f56bb07b163d041
ALEX BAIN <lxbain4@gmail.com>
2023-11-13 15:50:11 +1000

    this  commit

--- main.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-def BasicForm():
-    ids = StringField("ID",validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
